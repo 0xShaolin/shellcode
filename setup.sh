07e1636b57a4600e5db312a5733e197a9cb3287f
0xShaolin <odinsecurity@protonmail.com>
2018-10-13 13:19:47 -0500

    Initial Commit

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 echo "shellcode setup script!"
 
 echo "python2 ~/shellcode/shellcode.py" > shellcode
+chmod +x shellcode
 
 if uname -a | grep Android > /dev/null 2>&1 #termux
 then
 	mv shellcode ~/../usr/bin
 elif uname -a | grep Darwin > /dev/null 2>&1 #mac os
 then
-	mv shellcode /usr/local/bin
+	sudo mv shellcode /usr/local/bin
 elif uname -a | grep Linux > /dev/null 2>&1 #linux
 then
-	mv shellcode /usr/local/bin
+	sudo mv shellcode /usr/local/bin
 else
 	echo "architecture not found!"
 	exit
